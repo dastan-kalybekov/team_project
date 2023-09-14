e87c07cd763b68e1452a2778d79beb5ca40e20ec
unknown <dakedak670@gmail.com>
2023-09-14 12:11:32 +0600

    added comment serializer

--- post/serializers.py
+++ post/serializers.py
@@ -1,9 +1,21 @@
 from rest_framework import serializers
 
-from .models import Post, Comment
+from .models import Post, Comment, Rating
 
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
+        fields = '__all__'
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
+
+
+class RatingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Rating
         fields = '__all__'

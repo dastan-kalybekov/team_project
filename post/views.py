3e38f3808d56a60fa7973aadd2c007939cadb8a2
unknown <dakedak670@gmail.com>
2023-09-14 11:56:24 +0600

    update post view (save add user)

--- post/views.py
+++ post/views.py
@@ -9,3 +9,5 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)

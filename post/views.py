15b50497437a97ba82ec7b57acce4c846f4aa865
unknown <dakedak670@gmail.com>
2023-09-14 12:33:44 +0600

    added view comment and rating

--- post/views.py
+++ post/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
-from .serializers import PostSerializer
+from .serializers import PostSerializer, CommentSerializer, RatingSerializer
 
-from .models import Post
+from .models import Post, Comment, Rating
+
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class CommentViewSet(ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class RatingViewSet(ModelViewSet):
+    queryset = Rating.objects.all()
+    serializer_class = RatingSerializer
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
